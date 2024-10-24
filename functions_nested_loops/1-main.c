d3a9dd3926692b81d8445371e8cb661afde42740
rania-elh <rania1elhefnawy@gmail.com>
2024-10-24 17:26:38 +0200

    1

---
+++ functions_nested_loops/1-main.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+void print_alphabet(void);  // Déclaration de la fonctionint main(void)
+int main(void)
+{
+    print_alphabet();
+    return (0);
+}
