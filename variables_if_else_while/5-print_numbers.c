d8e600b0c6cd6fb1be4fe9ea9c1e1985f9bcd928
root <root@LAPTOP-NT03TL66>
2024-10-24 13:51:08 +0200

    5

--- variables_if_else_while/5-print_numbers.c
+++ variables_if_else_while/5-print_numbers.c
@@ -2,4 +2,20 @@
 #include <time.h>
 #include <stdio.h>
 
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 
+int main(void)
+{
+	int i;
+
+	for (i = 0 ; i < 10 ; i++)
+		printf("%d", i);
+	{
+	printf("\n");
+	}
+	return (0);
+}
