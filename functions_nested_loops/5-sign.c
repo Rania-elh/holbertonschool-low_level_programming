ac79593ab25d84318c860d3a340a7b58f394ca9a
rania-elh <rania1elhefnawy@gmail.com>
2024-10-25 15:20:06 +0200

    update 1

---
+++ functions_nested_loops/5-sign.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_sign - check if character is greater than zero, less than zero, zero.
+ * @n: the character to be checked.
+ * return value of print_sign.
+ * Return: 1 if n is greater than zero, 0 if n is zero, -1 otherwise.
+ * Print: + if n is greater than zero, 0 if n is zero, - otherwise
+ */
+
+int print_sign(int n)
+{
+	if (n > '0')
+		return (1);
+		_putchar('+')
+	else if ( n = '0' )
+		return (0)
+		_putchar('0')
+	else (
