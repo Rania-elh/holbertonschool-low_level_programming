56749ecfd234e9ab94bb2084d05edfb1f2a29fb1
rania-elh <rania1elhefnawy@gmail.com>
2024-10-25 12:21:36 +0200

    1

--- functions_nested_loops/1-main.c
+++ functions_nested_loops/1-main.c
@@ -1,17 +1,12 @@
-#include <stdio.h>
 #include "main.h"
 
 /**
  * main - check the code
- * print_alphabet - Affiche les lettres de l'alphabet en minuscules 
- * 
+ *
  * Return: Always 0.
  */
-
-void print_alphabet(void);
-
 int main(void)
 {
-    print_alphabet();
-    return (0);
+	print_alphabet();
+	return (0);
 }
