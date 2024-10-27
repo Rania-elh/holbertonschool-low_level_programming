c61698e31d38bdcfe1ffe6a054c075016d9c7acc
rania-elh <rania1elhefnawy@gmail.com>
2024-10-27 19:24:58 +0100

    10

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * add - check the result
+ * @a: the character to check
+ * @b: the character to check
+ * Return: the result of (a + b)
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
