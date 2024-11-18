21946efdc2b6b958aa88d16292a91afe5fa22e71
rania-elh <rania1elhefnawy@gmail.com>
2024-11-18 22:45:06 +0100

    4

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -1,35 +1,68 @@
-#include <string.h>
 #include "dog.h"
 #include <stdlib.h>
+#include <string.h>
 /**
-* new_dog -  function that creates a new dog
-* @name: the name of the dog
-* @age: the age of the dog
-* @owner: the owner of the dog
-* Return: pointer to the new dog
-*/
+ * _strlen - entry point
+ * @str: pointer to the string we need to count
+ * Description: calculate the length of a string
+ * Return: unsigned int if sucess -> length of the string.
+ */
+unsigned int _strlen(const char *str)
+{
+	unsigned int longueur = 0;
+
+	while (str[longueur] != '\0')
+		longueur++;
+
+	return (longueur);
+}
+/**
+ * _strdup - entry point
+ * @str: Pointer to the string we need to duplicate.
+ * Description: function that going to duplicate a specific string
+ * Return: Pointer to copy of the string if sucess
+ */
+char *_strdup(const char *str)
+{
+	unsigned int longueur, i;
+	char *new_str;
+
+	if (str == NULL)
+		return (NULL);
+
+	longueur = _strlen(str);
+
+	new_str = malloc(sizeof(char) * (longueur + 1));
+	if (new_str == NULL)
+		return (NULL);
+
+	for (i = 0; i <= longueur; i++)
+		new_str[i] = str[i];
+
+	return (new_str);
+}
+/**
+ * new_dog - entry point
+ * @name: pointer -> member name of the struct containing the name of the dog
+ * @age: age of the dog
+ * @owner: pointer -> member owner of the struct containing the owner's name
+ * Description: function that creates a new dog.
+ * Return: if success pointer to the new dog struct
+ */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	dog_t *newDog;
+	dog_t *nouveau_chien;
 
-	if (name == NULL || owner == NULL)
-	{
+	if (name == NULL || owner == NULL || age == 0)
+		return (0);
+
+	nouveau_chien = malloc(sizeof(dog_t));
+	if (nouveau_chien == NULL)
 		return (NULL);
-	}
-	newDog = malloc(sizeof(dog_t));
-	if (newDog == NULL)
-	{
-		return (NULL);
-	}
-	newDog->name = strdup(name);
-	newDog->owner = strdup(owner);
-	if (newDog->name == NULL || newDog->owner == NULL)
-	{
-		free(newDog->name);
-		free(newDog->owner);
-		free(newDog);
-		return (NULL);
-	}
-	newDog->age = age;
-	return (newDog);
+
+	nouveau_chien->name = _strdup(name);
+	nouveau_chien->owner = _strdup(owner);
+	nouveau_chien->age = age;
+
+	return (nouveau_chien);
 }
