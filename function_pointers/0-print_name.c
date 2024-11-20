241ad6eb5216da95436fa18045e2d30cff2b7697
rania-elh <rania1elhefnawy@gmail.com>
2024-11-20 15:35:01 +0100

    aymen

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -1,17 +1,15 @@
-#include <stdio.h>
+#include "function_pointers.h"
 
 /**
- * print_name -  prints a name
- * @name: nom
- * @f: jsp
- * Return: ALways 0 (Success)
+ * print_name - Prints a name.
+ * @name: The name to be printed.
+ * @f: A pointer to a function that prints a name.
  */
 
 void print_name(char *name, void (*f)(char *))
 {
-	if (name == NULL)
+	if (name == NULL || f == NULL)
 		return;
-	if (f == NULL)
-		return;
+
 	f(name);
 }
