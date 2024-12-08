5b33aa1a8487f32f836c03b61076be3dc8e8fc30
Rania-elh <rania1elhefnawy@gmail.com>
2024-12-08 22:46:00 +0000

    Projet 1

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -6,9 +6,8 @@
  * print_list - function to print a singly linked list
  * @h: pointer to list_t
  *
- * Return: number of node
+ * Return: number of nodes
  */
-
 size_t print_list(const list_t *h)
 {
 	size_t node = 0;
@@ -18,7 +17,7 @@ size_t print_list(const list_t *h)
 		if (h->str == NULL)
 			printf("[0] (nil)\n");
 		else
-			printf("[%d] %s\n", h->len, h->str);
+			printf("[%u] %s\n", h->len, h->str);
 		node++;
 		h = h->next;
 	}
